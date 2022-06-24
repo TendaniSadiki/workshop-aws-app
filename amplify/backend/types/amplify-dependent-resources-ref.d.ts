3911d0458fc040f391bcee2cf85bcadb1c85adef
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-24 16:41:03 +0200

    {first commit}

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "workshopawsapp05ec1f66": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
